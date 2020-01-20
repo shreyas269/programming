dd65bfd8196b7d57ecec0711dcb6960175904b01
Shreyas Kulkarni <shreyas.gp269@gmail.com>
2020-01-20 19:49:57 +0800

    problem update

---
+++ problems/spoj/adding_reverse_numbers.py
@@ -0,0 +1,16 @@
+def reverse(n):
+    n_str = str(n)
+    # a[start:stop:step] -> slice operator
+    return int(n_str[::-1])
+
+def main():
+    t = int(input())
+
+    for i in range(t):
+        n, m = input().split()
+        n, m = int(n), int(m)
+        sum = reverse(reverse(n) + reverse(m))
+        print(sum)
+
+if __name__ == "__main__":
+    main()
